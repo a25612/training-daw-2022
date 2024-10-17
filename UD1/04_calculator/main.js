dc561f2f737bba70c181defb7a84b2c01c1420c8
a25612 <a25612@svalero.com>
2024-10-17 12:22:02 +0200

    Ejercicio4

--- UD1/04_calculator/main.js
+++ UD1/04_calculator/main.js
@@ -3,11 +3,30 @@
 // Resta
 // Multiplicación
 // División
-
-
 // define variables
 let x, y;
+x = 4;
+y = 2;
+
+function suma (x, y){
+    return x + y;
+}
+
+function resta (x, y){
+    return x - y;
+}
+
+function multiplicacion(x, y){
+    return x * y;
+}
+
+function division(x, y){
+    return x / y;
+}
 
 
 // print sum, substract, multiply and divide
-console.log('something')
+console.log(`La suma es: ${suma(x, y)}`)
+console.log(`La resta es: ${resta(x, y)}`)
+console.log(`La multiplicacion es: ${multiplicacion(x, y)}`)
+console.log(`La division es: ${division(x, y)}`)
