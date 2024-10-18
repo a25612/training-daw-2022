55dbb5d9b5b8930ce951070dd95c7cb8b259c933
a25612 <a25612@svalero.com>
2024-10-18 10:10:10 +0200

    Ejercicio6

--- UD1/06_math_array/main.js
+++ UD1/06_math_array/main.js
@@ -1,18 +1,33 @@
-// Declarar un array numérico (entero) y crear un método que imprima:
-// La suma de todos los elementos
-// El elemento más grande
-// El elemento más pequeño
-// La media de los elementos
-
-
-
 function doCalculation(array) {
+    let sum = 0;
+    let max = array[0];
+    let min = array[0];
 
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i]; // Suma
 
+        // Comprobar máximo
+        if (array[i] > max) {
+            max = array[i];
+        }
+
+        // Comprobar mínimo
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+
+    // Calcular media
+    let average = sum / array.length;
+
+    // Imprimir resultados
+    console.log(`Suma: ${sum}`);
+    console.log(`Elemento más grande: ${max}`);
+    console.log(`Elemento más pequeño: ${min}`);
+    console.log(`Media: ${average}`);
+    console.log('----------');
 }
 
-
-
-doCalculation([1,2,3,4])
-doCalculation([5,5,5,5])
-doCalculation([1,1,1,2,2,2,3,3,3,4,4,4,5,5,5])
+doCalculation([1, 2, 3, 4]);
+doCalculation([5, 5, 5, 5]);
+doCalculation([1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5]);
