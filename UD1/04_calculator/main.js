cec37b8293b01e9d2b8a63d04b9fbde54effb58a
a25612 <a25612@svalero.com>
2024-10-18 09:59:54 +0200

    Comentarios Ejercicio4

--- UD1/04_calculator/main.js
+++ UD1/04_calculator/main.js
@@ -1,31 +1,30 @@
-// Declarar 2 variables con número e imprimir por consola (mediante console.log()) el valor de:
-// Suma
-// Resta
-// Multiplicación
-// División
 // define variables
 let x, y;
 x = 4;
 y = 2;
 
+// Suma
 function suma (x, y){
     return x + y;
 }
 
+// Resta
 function resta (x, y){
     return x - y;
 }
 
+// Multiplicación
 function multiplicacion(x, y){
     return x * y;
 }
 
+// División
 function division(x, y){
     return x / y;
 }
 
 
-// print sum, substract, multiply and divide
+// print suma, resta, multiplicacion y division
 console.log(`La suma es: ${suma(x, y)}`)
 console.log(`La resta es: ${resta(x, y)}`)
 console.log(`La multiplicacion es: ${multiplicacion(x, y)}`)
