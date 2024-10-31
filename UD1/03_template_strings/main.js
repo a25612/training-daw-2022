869b7ad96713041825fb6794b64ba02646d967e5
a25612 <a25612@svalero.com>
2024-10-31 09:24:18 +0100

    Ajuste ejercicio UD1 03

--- UD1/03_template_strings/main.js
+++ UD1/03_template_strings/main.js
@@ -6,9 +6,11 @@ let result = hello('John', 'Anderton', 35)
 if (result != "Hola John Anderton, tienes 35 años de edad") {
     console.log('test fail')
     return
+} else {
+    console.log('Test OK')
 }
 
-console.log('Test OK')
+
 
 //result = hello('John', 'Foo', 47)
 //if (result != "Hola John Foo, tienes 47 años de edad") {
