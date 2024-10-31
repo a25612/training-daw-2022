a7bb42d76a0f10105e88533270c2fc6f092d8fda
a25612 <a25612@svalero.com>
2024-10-31 10:10:17 +0100

    Ejercicio 6.02/6.table/7 y 8

--- UD3/06_modify_dom/table/main.js
+++ UD3/06_modify_dom/table/main.js
@@ -1,19 +1,31 @@
+let currentId = 1; // Variable autoincremental para el ID
+
 function onClick() {
-    let tbody = document.getElementsByTagName('tbody')[0]
+    const tbody = document.querySelector("tbody");
 
-    let tr = document.createElement('tr')
-    let td = document.createElement('td')
-    td.innerText = '0'
-    tr.appendChild(td)
+    const tr = document.createElement("tr");
 
-    td = document.createElement('td')
-    td.innerText = 'Example'
-    tr.appendChild(td)
+    let td = document.createElement("td");
+    td.innerText = currentId;
+    tr.appendChild(td);
 
-    td = document.createElement('td')
-    td.innerText = 'Surname'
-    tr.appendChild(td)
+    td = document.createElement("td");
+    td.innerText = "Example";
+    tr.appendChild(td);
 
-    tbody.appendChild(tr)
-    console.log('add')
+    td = document.createElement("td");
+    td.innerText = "Surname";
+    tr.appendChild(td);
+
+    // Columna de Acciones con enlace
+    td = document.createElement("td");
+    const enlace = document.createElement("a");
+    enlace.href = `http://localhost/edit/${currentId}`;
+    enlace.innerText = "Editar";
+    td.appendChild(enlace);
+    tr.appendChild(td);
+
+    tbody.appendChild(tr);
+
+    currentId++;
 }
