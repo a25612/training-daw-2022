0c523434fddded2969d64d12bbcb2059b0403c0f
a25612 <a25612@svalero.com>
2024-11-04 10:34:32 +0100

    Cambios ejercicio 03

--- UD1/03_template_strings/main.js
+++ UD1/03_template_strings/main.js
@@ -3,18 +3,9 @@ function hello(name, lastname, years) {
 }
 
 let result = hello('John', 'Anderton', 35)
-if (result != "Hola John Anderton, tienes 35 años de edad") {
-    console.log('test fail')
+if (result === "Hola John Anderton, tienes 35 años de edad") {
+    console.log(result)
     return
 } else {
-    console.log('Test OK')
+    console.log('Failed')
 }
-
-
-
-//result = hello('John', 'Foo', 47)
-//if (result != "Hola John Foo, tienes 47 años de edad") {
-//   console.log('test fail')
-//return
-//}
-
