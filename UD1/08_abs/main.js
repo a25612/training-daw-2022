30b3116946b367f0c13cb9d6aa7d23a352dff5ef
a25612 <a25612@svalero.com>
2024-11-04 10:52:57 +0100

    Ejercicio 07 y 08 de UD1

--- UD1/08_abs/main.js
+++ UD1/08_abs/main.js
@@ -1,9 +1,9 @@
 const assert = require('assert').strict;
 
 function abs(value) {
-    console.log(value);
-    return Math.abs(value)
 
+ console.log(value);
+    return Math.abs(value)
 }
 
 assert.strictEqual(abs(4), 4)
